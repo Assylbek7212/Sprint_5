b9bdd7914dae35e5bf41d3b861df16d983a62538
Assylbek Balmukhanov <assylbek.balmukhanov@gmail.com>
2024-08-16 22:46:21 +0200

    fix test of constructor

--- test/test_constructor.py
+++ test/test_constructor.py
@@ -1,24 +1,23 @@
-import time
-
 import pytest
 from locators import SignInMain
 from constants import name_site, active_tab_class
 
+
 class TestConstructor:
-    @pytest.mark.parametrize("tab",
-                             [SignInMain.SAUCES_TEXT,
-                              SignInMain.FILLINGS_TEXT,
-                              SignInMain.BUNS_TEXT])
-    def test_constructor_tab(self, driver, auth, tab):
+
+    @pytest.fixture
+    def navigate_to_home(self, driver, auth):
         auth(name_site, SignInMain.SUBMIT_BUTTON)
+        yield
 
-        if tab == SignInMain.BUNS_TEXT:
-            step = driver.find_element(*tab)
-            step_class = step.get_attribute('class')
-            assert step_class == active_tab_class, f"Expected '{active_tab_class}', but got '{step_class}'"
-        else:
-            driver.find_element(*tab).click()
+    def test_constructor_tab_buns(self, driver, navigate_to_home):
+        step = driver.find_element(*SignInMain.BUNS_TEXT)
+        step_class = step.get_attribute('class')
+        assert step_class == active_tab_class, f"Expected '{active_tab_class}', but got '{step_class}'"
 
-            step = driver.find_element(*tab)
-            step_class = step.get_attribute('class')
-            assert step_class == active_tab_class, f"Expected '{active_tab_class}', but got '{step_class}'"
+    @pytest.mark.parametrize("tab", [SignInMain.SAUCES_TEXT, SignInMain.FILLINGS_TEXT])
+    def test_constructor_other_tabs(self, driver, navigate_to_home, tab):
+        driver.find_element(*tab).click()
+        step = driver.find_element(*tab)
+        step_class = step.get_attribute('class')
+        assert step_class == active_tab_class, f"Expected '{active_tab_class}', but got '{step_class}'"
