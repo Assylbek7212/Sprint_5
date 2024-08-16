1e5b9d6bcff7cb6187ffa50e527062f03b6277ee
Assylbek Balmukhanov <assylbek.balmukhanov@gmail.com>
2024-08-16 22:36:23 +0200

    add expected_conditions

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,8 @@ import pytest
 from selenium import webdriver
 from locators import SignInMain
 from constants import *
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def driver():
@@ -18,5 +20,6 @@ def auth(driver):
         driver.find_element(*SignInMain.NAME_INPUT).send_keys(name_const)
         driver.find_element(*SignInMain.PASSWORD_INPUT).send_keys(password_const)
         driver.find_element(*SignInMain.SUBMIT_BUTTON_SIGN_IN).click()
-        time.sleep(1)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(SignInMain.SAUCES_TEXT))
+
     return _auth
